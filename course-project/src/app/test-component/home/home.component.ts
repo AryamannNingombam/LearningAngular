c7e10698f325912de3f5fbfbe85d842f45d4fdc3
ProDestroyer15 <aryamannsingh9@gmail.com>
2020-07-08 18:01:03 +0530

    Learned Observables

--- course-project/src/app/test-component/home/home.component.ts
+++ course-project/src/app/test-component/home/home.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { interval, Observable, Subscription, Observer } from 'rxjs';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-
+export class HomeComponent implements OnInit, OnDestroy {
+  private myObservable: Subscription
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    const customIntervalObservable = Observable.create(observer => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        count++;
+      }, 1000)
+    });
+    this.myObservable = customIntervalObservable.subscribe((data) => {
+      console.log(data);
+    });
+
+  }
+  ngOnDestroy() :void {
+    this.myObservable.unsubscribe();
   }
 
 }
